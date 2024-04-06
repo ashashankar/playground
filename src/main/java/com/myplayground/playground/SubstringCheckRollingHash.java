43761555475987856896ab643da786234990dc6d
@Ash <142460267+ashashankar29@users.noreply.github.com>
2024-04-05 22:09:18 -0700

    FindingSubstring using polynomialrollinghash

---
+++ src/main/java/com/myplayground/playground/SubstringCheckRollingHash.java
@@ -0,0 +1,13 @@
+package com.myplayground.playground;
+
+public class SubstringCheckRollingHash {
+    public static void main(String[] args) {
+        String input = "ABDCBAIBDCSQ";
+        String find = "CBA";
+    }
+
+    public boolean findSubstring(String input, String find) {
+
+        return false;
+    }
+}
